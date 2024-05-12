5932842f0080a8b24a06ad71c0a498c87e968022
Lebackrobot <lebackman18@gmail.com>
2024-05-12 19:19:50 -0300

    ⚙️ env: add cloud database

--- src/schemas/ong.schema.js
+++ src/schemas/ong.schema.js
@@ -3,6 +3,7 @@ import Joi from 'joi'
 const ongCreateSchema = Joi.object({
     name: Joi.string().required(),
     description: Joi.string().required(),
+    email: Joi.string().required(),
     createdAt: Joi.date().iso().allow(null),
     updatedAt: Joi.date().iso().allow(null)
 })
