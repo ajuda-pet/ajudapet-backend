8dd0621b04da848e34dc6d64948b2436e16232ad
Lebackrobot <lebackman18@gmail.com>
2024-05-15 14:34:46 -0300

    🗃️ migration: add pet relation

--- config/prisma/migrations/20240515172921_create_table_pets/migration.sql
+++ config/prisma/migrations/20240515172921_create_table_pets/migration.sql
@@ -13,6 +13,7 @@ CREATE TYPE "speciesPetEnum" AS ENUM ('CAT', 'DOG');
 -- CreateTable
 CREATE TABLE "pets" (
     "id" SERIAL NOT NULL,
+    "adoption_point_id" INTEGER NOT NULL,
     "name" TEXT NOT NULL,
     "description" TEXT,
     "availibility_adotpion" BOOLEAN NOT NULL,
@@ -26,3 +27,6 @@ CREATE TABLE "pets" (
 
     CONSTRAINT "pets_pkey" PRIMARY KEY ("id")
 );
+
+-- AddForeignKey
+ALTER TABLE "pets" ADD CONSTRAINT "pets_adoption_point_id_fkey" FOREIGN KEY ("adoption_point_id") REFERENCES "adoption_points"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
