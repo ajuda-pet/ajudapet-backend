14a389ebf5576e78fa3e05835462cdfa3e8ab606
Lebackrobot <lebackman18@gmail.com>
2024-05-18 03:06:47 -0300

    ✨ feat: signup by invite

--- src/routes/noauth/signup.router.js
+++ src/routes/noauth/signup.router.js
@@ -1,8 +1,12 @@
 import { Router } from 'express'
 import signupController from '../../controllers/signup.controller.js'
+import inviteMiddleware from '../auth/middlewares/invite.middleware.js'
 
 const signupRouter = Router()
 
 signupRouter.post('/signup', signupController.create)
+signupRouter.post('/signup/invite', inviteMiddleware, signupController.createByInvite)
+
+
 
 export default signupRouter
