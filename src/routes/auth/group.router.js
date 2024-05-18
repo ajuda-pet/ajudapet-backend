3e7c8fc45948d4d313af5b660fd41425a4e2a61e
Lebackrobot <lebackman18@gmail.com>
2024-05-18 01:40:48 -0300

    ✨ feat: make invite

--- src/routes/auth/group.router.js
+++ src/routes/auth/group.router.js
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import groupController from '../../controllers/group.controller.js'
 import socialMediaController from '../../controllers/social-media.controller.js'
+import inviteController from '../../controllers/invite.controller.js'
 
 const authGroupRouter = Router()
 
@@ -9,5 +10,7 @@ authGroupRouter.delete('/groups', groupController.delete)
 authGroupRouter.post('/groups/social-media', socialMediaController.create)
 authGroupRouter.put('/groups/social-media/:plataform', socialMediaController.update)
 authGroupRouter.delete('/groups/social-media/:plataform', socialMediaController.delete)
+
+authGroupRouter.get('/groups/invite', inviteController.get)
 
 export default authGroupRouter
