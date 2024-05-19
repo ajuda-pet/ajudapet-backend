a49443ce4a9f192e491812baf738f0f4a91db971
JulianadFreitas <julianachristello@gmail.com>
2024-05-19 14:49:39 -0300

    feat: add no_auth router

---
+++ src/routes/noauth/pix.router.js
@@ -0,0 +1,28 @@
+import { Router } from 'express';
+
+import pixController from '../../controllers/pix.controller.js';
+
+
+
+
+const noAuthPixRouter = Router();
+
+
+
+
+// Rotas para o PIX
+
+noAuthPixRouter.get('/pix/:group_id', pixController.get);
+
+noAuthPixRouter.get('/pix/:group_id/:id', pixController.getById);
+
+noAuthPixRouter.post('/pix/:group_id', pixController.create);
+
+noAuthPixRouter.put('/pix/:group_id/:id', pixController.update);
+
+noAuthPixRouter.delete('/pix/:group_id/:id', pixController.delete);
+
+
+
+
+export default noAuthPixRouter;
