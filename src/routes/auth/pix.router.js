7456f111d129204b3c73b6b443706d3f0aae6906
JulianadFreitas <julianachristello@gmail.com>
2024-05-19 18:20:25 -0300

    fix: resolver snake case

--- src/routes/auth/pix.router.js
+++ src/routes/auth/pix.router.js
@@ -3,10 +3,9 @@ import pixController from '../../controllers/pix.controller.js';
 
 const authPixRouter = Router();
 
-authPixRouter.get('/groups/:group_id/pix', pixController.get);
-authPixRouter.get('/groups/:group_id/pix/:id', pixController.getById);
-authPixRouter.post('/groups/:group_id/pix', pixController.create);
-authPixRouter.put('/groups/:group_id/pix/:id', pixController.update);
-authPixRouter.delete('/groups/:group_id/pix/:id', pixController.delete);
+authPixRouter.get('/groups/:groupId/pix', pixController.get);
+authPixRouter.post('/groups/:groupId/pix', pixController.create);
+authPixRouter.put('/groups/:groupId/pix', pixController.update);
+authPixRouter.delete('/groups/:groupId/pix', pixController.delete);
 
 export default authPixRouter;
